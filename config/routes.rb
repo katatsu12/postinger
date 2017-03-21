4b59b14b97080480ef6013a57d79c037864caeab
alexckua <alex@net.ck.ua>
2017-03-21 11:53:40 +0200

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :posts
   root 'posts#index'
+  get '/', to: 'post#index'
 end
