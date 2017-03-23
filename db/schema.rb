f6267f464174cc5a6e99818484a781b98574a039
alexckua <alex@net.ck.ua>
2017-03-23 14:40:33 +0200

    enabled sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,29 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20170322215624) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "accounts", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password"
-    t.string   "login"
-    t.string   "twitter"
-    t.string   "facebook"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "user_id"
-    t.index ["user_id"], name: "index_accounts_on_user_id", using: :btree
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,8 +39,8 @@ ActiveRecord::Schema.define(version: 20170322215624) do
     t.string   "account_id"
     t.string   "token_twitter"
     t.string   "secret_twitter"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
